58d195cfa0501a0be196311de1acdd2ae9d345af
nevashe <nevashe@bk.ru>
2021-12-29 20:04:30 +0300

    hm6

---
+++ hm6/src/main/java/hm6/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package hm6.controllers;
+
+import hm6.entities.Product;
+import hm6.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public void setStudentService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("")
+    public String shopPage(Model model) {
+        List<Product> allProducts = productService.getAllProducts();
+        model.addAttribute("products", allProducts);
+        return "products";
+    }
+}
