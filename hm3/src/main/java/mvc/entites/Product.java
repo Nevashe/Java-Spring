ee49ad783c3454a91a8c2302108a7e380bd3d9cd
nevashe <nevashe@bk.ru>
2022-01-06 17:31:07 +0300

    hm8

--- hm3/src/main/java/mvc/entites/Product.java
+++ hm3/src/main/java/mvc/entites/Product.java
@@ -1,40 +1,40 @@
-package mvc.entites;
-
-public class Product {
-    private int id;
-    private String title;
-    private int cost;
-
-    public Product(int id, String title, int cost) {
-        this.id = id;
-        this.title = title;
-        this.cost = cost;
-    }
-
-    public Product() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-}
+//package mvc.entites;
+//
+//public class Product {
+//    private int id;
+//    private String title;
+//    private int cost;
+//
+//    public Product(int id, String title, int cost) {
+//        this.id = id;
+//        this.title = title;
+//        this.cost = cost;
+//    }
+//
+//    public Product() {
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public int getCost() {
+//        return cost;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public void setCost(int cost) {
+//        this.cost = cost;
+//    }
+//}
